58ebe77b35a05aa466ee9b84a05f26b88d030236
Ichsan Hariadi <pythonjokeun@gmail.com>
2019-08-04 23:31:20 +0700

    Generating requirements.txt on the fly

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,8 @@
 """Set of hooks after project is generated."""
 
-print("\n{{ cookiecutter.service_name }} project successfully created ✨")
-print("\nHave a nice development 🍻")
-print("")
+import os
+
+os.system("pipenv install --dev")
+
+print("{{ cookiecutter.service_name }} project successfully created ✨")
+print("Have a nice development 🍻")
