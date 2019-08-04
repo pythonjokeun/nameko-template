697e39099878471381383eb2827055db5010954f
Ichsan Hariadi <pythonjokeun@gmail.com>
2019-08-05 00:09:11 +0700

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,7 +2,9 @@
 
 import os
 
+print("Installing initial dependencies via Pipenv 📝")
 os.system("pipenv install --dev")
 
-print("{{ cookiecutter.service_name }} project successfully created ✨")
+print("'{{ cookiecutter.service_name }}' project successfully created ✨")
+print("To start working on the new project, just change your current directory to '{{ cookiecutter.service_name }}' 🛠")
 print("Have a nice development 🍻")
