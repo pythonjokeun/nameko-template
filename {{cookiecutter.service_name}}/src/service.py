316f5fcc6269c14816b74a71114dd419c039c950
Ichsan Hariadi <pythonjokeun@gmail.com>
2019-08-05 13:44:15 +0700

    Select service_extension

--- {{cookiecutter.service_name}}/src/service.py
+++ {{cookiecutter.service_name}}/src/service.py
@@ -1,21 +1,52 @@
 """Module description goes here."""
-
+{% if cookiecutter.service_extension == "RPC" %}
 from nameko.rpc import rpc
-
+{% elif cookiecutter.service_extension == "HTTP" %}
+from nameko.web.handlers import http
+{% endif %}
 
 class {{ cookiecutter.service_name.title().replace("_", "") }}:
     """Service's description goes here."""
 
     name = "{{ cookiecutter.service_name }}"
-
+    {% if cookiecutter.service_extension == "RPC" %}
     @rpc
     def foo(self):
-        """Service's method description goes here."""
+        """Service's method description goes here.
+
+        For more information:
+        https://nameko.readthedocs.io/en/stable/built_in_extensions.html#rpc
+        """
 
         return "foo"
 
     @rpc
     def bar(self):
-        """Service's method description goes here."""
+        """Service's method description goes here.
+
+        For more information:
+        https://nameko.readthedocs.io/en/stable/built_in_extensions.html#rpc
+        """
 
         return "bar"
+    {% elif cookiecutter.service_extension == "HTTP" %}
+    @http("GET", "/foo")
+    def foo(self, request):
+        """Service's method description goes here.
+
+        For more information:
+        https://nameko.readthedocs.io/en/stable/built_in_extensions.html#http
+        """
+
+        return "foo"
+
+    @http("POST", "/bar")
+    def bar(self, request):
+        """Service's method description goes here.
+
+        For more information:
+        https://nameko.readthedocs.io/en/stable/built_in_extensions.html#http
+        """
+
+        return "bar"
+    {% endif %}
